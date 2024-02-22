defa09351a58c5fb4aca0d3c2c690d31c84af9c0
Chandan-Vishwakarma1 <chandan807cv@gmail.com>
2024-02-22 12:22:07 +0530

    Initial commit

---
+++ src/main/java/com/fitness/app/FitnessApplicaton.java
@@ -0,0 +1,13 @@
+package com.fitness.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessApplicaton {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessApplicaton.class, args);
+	}
+
+}
